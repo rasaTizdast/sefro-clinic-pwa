0f9593b0fb904f3db2c3385555f05d5d2c8b9ce1
Rasa Tizdast <rasaknifehand@gmail.com>
2026-06-23 17:31:57 +0330

    chore(config): add path aliases, new deps, and eslint ignore for backend

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores(["dist", "backend"]),
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
