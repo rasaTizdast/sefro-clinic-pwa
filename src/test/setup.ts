18f2d68f8cd2f521652cbcce704c78bd401e9ee9
rasaTizdast <rasaknifehand@gmail.com>
2026-06-25 01:26:40 +0330

    test: add HTMLDialogElement polyfill for test environment
    - Add showModal/close polyfill for dialog elements in tests

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,10 @@
 import "@testing-library/jest-dom/vitest";
+
+if (!HTMLDialogElement.prototype.showModal) {
+  HTMLDialogElement.prototype.showModal = function () {
+    this.open = true;
+  };
+  HTMLDialogElement.prototype.close = function () {
+    this.open = false;
+  };
+}
